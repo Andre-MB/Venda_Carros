8683ab7e739e662fd104574c467d89d13d5d1519
Giselle Cristina <gisellepereira1207@gmail.com>
2024-04-20 22:29:56 -0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
 
-const nameAdd = document.querySelector('#name-add');
-const yearAdd = document.querySelector('#year-add');
-const priceAdd = document.querySelector('#price-add')
-
-function create(e){
-    
-    e.preventDefault()
-    const name = document.querySelector('#name');
-    //const price = document.querySelector('#price');
-    //const year = document.querySelector('#year');
-    
-
-    name.innerHTML = nameAdd.value
-
-}
 
 
-
-document.querySelector('button').addEventListener('click',create)
