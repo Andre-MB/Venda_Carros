3fc2e3dbab557317c89f72edc8158548e3671474
Giselle Cristina <gisellepereira1207@gmail.com>
2024-04-21 08:20:17 -0300

    add some-animation

--- src/index.js
+++ src/index.js
@@ -5,11 +5,21 @@ const bike = document.querySelector('#bike');
 
 const nameCategory = document.querySelector('#name-category')
 
+    nav[0].classList.add('active')
+
 nav.forEach((e)=>{
     e.addEventListener('click',()=>{
         document.querySelector('.active')?.classList.remove('active')
         e.classList.add('active')
-        console.log(e)
+        if(e == nav[0]){
+            nameCategory.innerHTML = 'Catalogo Carro';
+        }else if(e == nav[1]){
+            nameCategory.innerHTML = 'Catalogo Motos';
+        }else if(e == nav[2]){
+            nameCategory.innerHTML = 'Catalogo Bicicletas';
+        }
     })
 })
+
+console.log(nav)
 
