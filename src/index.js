e022dff8a917fe0fc834fdcfa03f8ed276f88872
Giselle Cristina <gisellepereira1207@gmail.com>
2024-04-20 23:29:41 -0300

    updates js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const nav = document.querySelectorAll('.nav-itens');
+const moto = document.querySelector('#moto');
+const car = document.querySelector('#car');
+const bike = document.querySelector('#bike');
 
+const nameCategory = document.querySelector('#name-category')
 
+nav.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        document.querySelector('.active')?.classList.remove('active')
+        e.classList.add('active')
+        console.log(e)
+    })
+})
 
